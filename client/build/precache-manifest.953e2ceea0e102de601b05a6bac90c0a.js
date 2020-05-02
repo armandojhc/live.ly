11a8087a264d82a0852e0d36e3a3ab7c03c18482
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-05-01 22:03:13 -0400

    added icon images, updated the index.html favicon, manifest

---
+++ client/build/precache-manifest.953e2ceea0e102de601b05a6bac90c0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cdfc8101401bff302a9",
+    "url": "/static/css/main.334dd2cd.chunk.css"
+  },
+  {
+    "revision": "2cdfc8101401bff302a9",
+    "url": "/static/js/main.df451185.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "65694cfda82bbc181044ee313e6dda0d",
+    "url": "/index.html"
+  }
+];
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+// import Navbar from '../src/components/navbar';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				{/* <Navbar /> */}
+				<div>hello Team</div>
+			</div>
+		);
+	}
 }
 
 export default App;
