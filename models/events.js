b3cdf9185daed9aa322493939da577089b16387c
Armando Herrera Canelon <armando.herrera91@gmail.com>
2020-05-05 00:26:39 -0400

    created models

---
+++ models/events.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+var eventSchema = new Schema ({
+    eventPhotoURL: { type: String, required: true },
+  gifURL: { type: String, default: "" },
+  name: { type: String, required: true },
+  eventDate: { type: Date } ,
+  eventLink: { type: String },
+//   eventOwner: {type: mongoose.Types.ObjectId}
+});
+
+const Event = mongoose.model("Events", eventSchema);
+
+
+module.exports = Event;
