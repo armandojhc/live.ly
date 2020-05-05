dd703c76ddf6b05f8e25eb0871a29150dbaf6a39
XikkeN <e.humphreys@tricentis.com>
2020-05-05 19:41:30 -0400

    plz work

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import Feeds from '../components/feedsEvent/index';
 import TrendingEvents from '../components/trendingEvent/index';
 import FutureEvents from '../components/futureEvent/index';
-import Footer from '../components/footer/index';
 
-function HomePage() {
-	return (
-		<div>
-			<Feeds />
-			<TrendingEvents />
-			<FutureEvents />
-			<Footer />
-		</div>
-	);
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		padding: theme.spacing(2),
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+	},
+}));
+
+export default function CenteredGrid() {
+	const classes = useStyles(); {
+		return (
+			<div>
+				<Grid container spacing={3}>
+					<Grid item xs={12}>
+						<Paper className={classes.paper}>
+							<TrendingEvents />
+						</Paper>
+					</Grid>
+					<Grid item xs={9}>
+						<Paper className={classes.paper}>
+							<Feeds />
+						</Paper>
+					</Grid>
+					<Grid item xs={3}>
+						<Paper className={classes.paper}>
+							<FutureEvents />
+						</Paper>
+					</Grid>
+					</Grid>
+			</div>
+		);
+	}
 }
-
-export default HomePage;
