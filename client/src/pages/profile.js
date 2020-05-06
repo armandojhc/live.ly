da7a6911e5166e6f82d764f5703c09d0657d3263
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-05-05 20:39:10 -0400

    updated the profile page with container

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Feeds from '../components/feedsEvent/index';
 import Footer from '../components/footer/index';
+import Grid from '@material-ui/core/Grid';
 
 function Profile() {
 	return (
 		<div>
-			<Feeds />
-			<Footer />
+			<Grid container spacing={3}>
+				<Feeds />
+				<Footer />
+			</Grid>
 		</div>
 	);
 }
