1d26dc84a2475841fc0eda5520c7f30c3f305bad
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-05-05 21:00:34 -0400

    updated the container position

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -2,16 +2,38 @@ import React from 'react';
 import Feeds from '../components/feedsEvent/index';
 import Footer from '../components/footer/index';
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+	root  : {
+		flexGrow : 1
+	},
+	paper : {
+		padding   : theme.spacing(2),
+		textAlign : 'center',
+		color     : theme.palette.text.secondary
+	}
+}));
 
 function Profile() {
-	return (
-		<div>
-			<Grid container spacing={3}>
-				<Feeds />
-				<Footer />
-			</Grid>
-		</div>
-	);
+	const classes = useStyles();
+	{
+		return (
+			<div>
+				<Container maxWidth="md">
+					<Grid container spacing={3}>
+						<Grid item xs={12}>
+							<Paper className={classes.paper}>
+								<Feeds />
+							</Paper>
+						</Grid>
+					</Grid>
+				</Container>
+			</div>
+		);
+	}
 }
 
 export default Profile;
