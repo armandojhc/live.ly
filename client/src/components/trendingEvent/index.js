106014a89338c6961f3377d1ad03cce53bf42b8d
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-05-15 15:51:21 -0400

    added the live icon to the trending events

--- client/src/components/trendingEvent/index.js
+++ client/src/components/trendingEvent/index.js
@@ -5,66 +5,73 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
+import './index.css';
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		display: 'flex',
-		flexWrap: 'wrap',
-		justifyContent: 'space-around',
-		overflow: 'hidden',
-		backgroundColor: theme.palette.background.paper,
+	root     : {
+		display         : 'flex',
+		flexWrap        : 'wrap',
+		justifyContent  : 'space-around',
+		overflow        : 'hidden',
+		backgroundColor : theme.palette.background.paper
 	},
-	gridList: {
-		flexWrap: 'nowrap',
+	gridList : {
+		flexWrap  : 'nowrap',
 		// Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-		transform: 'translateZ(0)',
+		transform : 'translateZ(0)'
 	},
-	title: {
-		color: theme.palette.primary.white,
+	title    : {
+		color : theme.palette.primary.white
 	},
-	titleBar: {
-		background:
-			'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-	},
+	titleBar : {
+		background :
+			'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)'
+	}
 }));
 
 export default function SingleLineGridList() {
 	const classes = useStyles();
-	const [artistData, setArtistData] = React.useState([]);
-	const [refreshEvents, setRefreshEvents] = React.useState(true);
+	const [ artistData, setArtistData ] = React.useState([]);
+	const [ refreshEvents, setRefreshEvents ] = React.useState(true);
 
-	useEffect(() => {
-
-		//Get the data from the server
-		console.log("Getting data...");
-		fetch("/api/event")
-		.then(res => res.json())
-		.then(json => {
-		  console.log(json);
-		  setRefreshEvents(false);
-		  setArtistData(json);
-		})
-		.catch(err => {
-		  console.log(err);
-		})
-	  }, [refreshEvents])
+	useEffect(
+		() => {
+			//Get the data from the server
+			console.log('Getting data...');
+			fetch('/api/event')
+				.then((res) => res.json())
+				.then((json) => {
+					console.log(json);
+					setRefreshEvents(false);
+					setArtistData(json);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+		[ refreshEvents ]
+	);
 
 	return (
 		<div className={classes.root}>
 			<GridList className={classes.gridList} cols={2.5}>
-				{
-				artistData.map((event) => (
+				{artistData.map((event) => (
 					<GridListTile key={event.gifURL}>
 						<img src={event.gifURL} alt={event.eventOwner.name} />
+
 						<GridListTileBar
 							title={event.eventOwner.name}
 							classes={{
-								root: classes.titleBar,
-								title: classes.title,
+								root  : classes.titleBar,
+								title : classes.title
 							}}
 							actionIcon={
-								<IconButton aria-label={`star ${event.eventOwner.name}`}>
-									<StarBorderIcon className={classes.title} />
+								<IconButton
+									aria-label={`star ${event.eventOwner.name}`}
+									className="iconStyling"
+								>
+									<LiveTvIcon className="liveIcon" />
 								</IconButton>
 							}
 						/>
